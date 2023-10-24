5f38a2a5caf86243984d2d02065c382b92f94f5e
Romualdus17 <145730918+Romualdus17@users.noreply.github.com>
2023-10-24 19:54:30 +0700

    Add files via upload
    add file

---
+++ Portfolio_Upgrade/animated-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return <div>
+    <Section>Hero</Section>
+    <Section>Parallax</Section>
+    <Section>Services</Section>
+    <Section>Parallax</Section>
+    <Section>Portfolio1</Section>
+    <Section>portfolio2</Section>
+    <Section>Portfolio3</Section>
+    <Section>Contact</Section>
+  </div>;
+};
+
+export default App;
